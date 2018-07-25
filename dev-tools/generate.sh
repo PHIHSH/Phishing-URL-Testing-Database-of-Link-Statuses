f67d2d22c25c1be8942264ce6688169e1a71ba5e
funilrys <contact@funilrys.com>
2018-07-25 17:37:16 +0200

    Fix issue regarding PyFunceble update.
    Indeed, without this patch, we were not fetching the latest configuration file.

--- dev-tools/generate.sh
+++ dev-tools/generate.sh
@@ -4,6 +4,7 @@
 # Copyright Mitchell Krog - mitchellkrog@gmail.com
 
 input=${TRAVIS_BUILD_DIR}/input-source/ALL-feeds.list
+pyfuncebleConfigurationFileLocation=${TRAVIS_BUILD_DIR}/dev-tools/.PyFunceble.yaml
 
 
 # **********************
@@ -21,6 +22,12 @@ RunFunceble () {
     cd ${TRAVIS_BUILD_DIR}/dev-tools
 
     hash PyFunceble
+
+    if [[ -f "${pyfuncebleConfigurationFileLocation}" ]]
+    then
+        rm "${pyfuncebleConfigurationFileLocation}"
+    fi
+
     PyFunceble --travis -dbr 5 --cmd-before-end "bash ${TRAVIS_BUILD_DIR}/dev-tools/commit.sh" -ex --plain --autosave-minutes 10 --commit-autosave-message "V0.1.${TRAVIS_BUILD_NUMBER} [PyFunceble]" --commit-results-message "V0.1.${TRAVIS_BUILD_NUMBER}" --url-file ${input}
 
 }
