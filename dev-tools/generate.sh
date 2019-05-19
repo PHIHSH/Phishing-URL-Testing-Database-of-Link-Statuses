9a7bda801a2406580fe6fbc7781c5088f2b73665
Mitchell Krog <mitchellkrog@gmail.com>
2019-05-19 12:39:16 +0200

    Fix typo

--- dev-tools/generate.sh
+++ dev-tools/generate.sh
@@ -3,7 +3,7 @@
 # REPO: https://github.com/mitchellkrogza/Phishing.Database
 # Copyright Mitchell Krog - mitchellkrog@gmail.com
 
-input=${TRAVIS_BUILD_DIR}/input-source/ALL-feeds.list
+input=${TRAVIS_BUILD_DIR}/input-source/ALL-feeds-URL.list
 pyfuncebleConfigurationFileLocation=${TRAVIS_BUILD_DIR}/dev-tools/.PyFunceble.yaml
 pyfuncebleProductionConfigurationFileLocation=${TRAVIS_BUILD_DIR}/dev-tools/.PyFunceble_production.yaml
 
