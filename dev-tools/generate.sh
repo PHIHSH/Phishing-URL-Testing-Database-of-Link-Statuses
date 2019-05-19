fed7e9e5c28c17725bccc1ea48efa228c079bac2
Mitchell Krog <mitchellkrog@gmail.com>
2019-05-19 19:06:36 +0200

    Reduce Build Time

--- dev-tools/generate.sh
+++ dev-tools/generate.sh
@@ -30,7 +30,7 @@ RunFunceble () {
         rm "${pyfuncebleProductionConfigurationFileLocation}"
     fi
 
-    PyFunceble --travis -dbr 5 --multiprocess --processes 60 --dns 1.1.1.1 1.0.0.1 --quiet -ex --plain --cmd-before-end "bash ${TRAVIS_BUILD_DIR}/dev-tools/commit.sh" --autosave-minutes 10 --commit-autosave-message "V0.1.${TRAVIS_BUILD_NUMBER} [PyFunceble]" --commit-results-message "V0.1.${TRAVIS_BUILD_NUMBER}" --url-file ${input}
+    PyFunceble --travis -dbr 5 --multiprocess --processes 60 --dns 1.1.1.1 1.0.0.1 -ex --plain --cmd-before-end "bash ${TRAVIS_BUILD_DIR}/dev-tools/commit.sh" --autosave-minutes 5 --commit-autosave-message "V0.1.${TRAVIS_BUILD_NUMBER} [PyFunceble]" --commit-results-message "V0.1.${TRAVIS_BUILD_NUMBER}" --url-file ${input}
 
 }
 
